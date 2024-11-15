f4aa36fe4eeb2f4448abb2c4aff75d04292df306
victordelajarte <29466815+victordelajarte@users.noreply.github.com>
2024-11-15 12:27:52 +0100

    fix: add try catch & rename route

--- index.ts
+++ index.ts
@@ -1,33 +1,36 @@
 import multipart from '@fastify/multipart';
 import fastify from 'fastify';
-import { writeFileSync } from 'fs';
 import { renderPageAsImage } from 'unpdf';
 
 const server = fastify().register(multipart);
 
-server.post('/', async (request, response) => {
-    console.log('Converting PDF to image');
-    const parts = await request.file();
-    if (!parts) {
-        console.log('No parts found');
-        throw new Error('No parts found');
+server.post('/convert', async (request, response) => {
+    try {
+        console.log('Converting PDF to image');
+        const parts = await request.file();
+        if (!parts) {
+            console.log('No parts found');
+            throw new Error('No parts found');
+        }
+
+        const chunks = [];
+        for await (const chunk of parts.file) {
+            chunks.push(chunk);
+        }
+        console.log('Got chunks', chunks.length);
+
+        const pdfBuffer = Buffer.concat(chunks);
+        const image = await renderPageAsImage(new Uint8Array(pdfBuffer), 1, {
+            canvas: () => import("canvas"),
+        });
+        const imageBuffer = Buffer.from(image);
+        response.header('Content-Type', 'image/png');
+
+        response.send(imageBuffer);
+    } catch (error) {
+        console.error({ error });
+        response.status(500).send({ error });
     }
-
-    const chunks = [];
-    for await (const chunk of parts.file) {
-        chunks.push(chunk);
-    }
-
-    const pdfBuffer = Buffer.concat(chunks);
-    const image = await renderPageAsImage(new Uint8Array(pdfBuffer), 1, {
-        canvas: () => import("canvas"),
-    });
-    const imageBuffer = Buffer.from(image);
-    response.header('Content-Type', 'image/png');
-
-    writeFileSync('./output/image.png', imageBuffer);
-
-    response.send(imageBuffer);
 })
 
 server.listen({ port: 5000 }, (err, address) => {
